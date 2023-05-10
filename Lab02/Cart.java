e2171899dfa5d9cd54879c373dd53b0f9be81c8f
IamVDD <vodinhdat03@gmail.com>
2023-05-10 11:27:36 +0700

    add lab02

---
+++ Lab02/Cart.java
@@ -0,0 +1,11 @@
+public class Cart {
+    int qtyOrdered;
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+
+    }
+    
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        
+    }
+}
